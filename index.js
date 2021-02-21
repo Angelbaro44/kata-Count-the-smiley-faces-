7ae57c5628e364d0d2d46368214304e5a7233cf1
Angel Baro <angelbaro44@gmail.com>
2021-02-20 22:57:10 -0500

    kata finished

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 function countSmileys(arr) {
-      var counter = 0;
-      
-      for (var i=0; i<arr.length; i++){
+var faceArr = [":)",";)",":-)",";-)",";~)",":~)",":D",";D",":-D",":~D",";-D",";~D"];
+var counter = 0;
 
-        }
-      return counter;
-      }
+for (var i=0; i<arr.length; i++){
+  for (var j=0; j<faceArr.length; j++){
+    if (arr[i]===faceArr[j]){
+      counter++;
+    }
+  }
+  }
+return counter;
+}

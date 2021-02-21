3c7defada46330808c973100560a8ec1e05ee476
Angel Baro <angelbaro44@gmail.com>
2021-02-20 23:05:57 -0500

    added notes

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+// This solution iterates thru the given arr 
+// then compares it to our approved smilies.
+// If there is a match then the counter gets bumped up and returned!
+
 function countSmileys(arr) {
 var faceArr = [":)",";)",":-)",";-)",";~)",":~)",":D",";D",":-D",":~D",";-D",";~D"];
 var counter = 0;
@@ -11,3 +15,14 @@ for (var i=0; i<arr.length; i++){
   }
 return counter;
 }
+
+
+// Test Cases
+console.log(countSmileys([])) // Expected Answer: 0
+console.log(countSmileys([':)', ';(', ';}', ':-D'])) // Expected Answer: 2
+console.log(countSmileys([';]', ':[', ';*', ':$', ';-D'])) // Expected Answer: 1
+console.log(countSmileys([':)',':(',':D',':O',':;'])) // Expected Answer: 2
+console.log(countSmileys([':-)',';~D',':-D',':_D'])) // Expected Answer: 3
+console.log(countSmileys([':---)','))',';~~D',';D'])) // Expected Answer: 1
+console.log(countSmileys([';~)',':)',':-)',':--)'])) // Expected Answer: 3
+console.log(countSmileys([':o)',':--D',';-~)'])) // Expected Answer: 0

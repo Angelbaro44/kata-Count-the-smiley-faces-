591be975dcf3ff2fb7f76ca155382d076431d9ce
Angel Baro <angelbaro44@gmail.com>
2021-02-20 23:10:18 -0500

    Added alt solution

--- index.js
+++ index.js
@@ -3,11 +3,11 @@
 // If there is a match then the counter gets bumped up and returned!
 
 function countSmileys(arr) {
-var faceArr = [":)",";)",":-)",";-)",";~)",":~)",":D",";D",":-D",":~D",";-D",";~D"];
-var counter = 0;
+let faceArr = [":)",";)",":-)",";-)",";~)",":~)",":D",";D",":-D",":~D",";-D",";~D"];
+let counter = 0;
 
-for (var i=0; i<arr.length; i++){
-  for (var j=0; j<faceArr.length; j++){
+for (let i=0; i<arr.length; i++){
+  for (let j=0; j<faceArr.length; j++){
     if (arr[i]===faceArr[j]){
       counter++;
     }
@@ -26,3 +26,32 @@ console.log(countSmileys([':-)',';~D',':-D',':_D'])) // Expected Answer: 3
 console.log(countSmileys([':---)','))',';~~D',';D'])) // Expected Answer: 1
 console.log(countSmileys([';~)',':)',':-)',':--)'])) // Expected Answer: 3
 console.log(countSmileys([':o)',':--D',';-~)'])) // Expected Answer: 0
+
+
+
+
+
+
+
+
+// // ALT SOLUTION
+
+// // This is a one liner regex solution 
+// const faceArr = /[:;]{1}[-~]?[)D]{1}/;
+// const countSmileys = (arr) => arr.filter(face => faceArr.test(face)).length;
+
+
+
+
+// // Test Cases
+// console.log(countSmileys([])) // Expected Answer: 0
+// console.log(countSmileys([':)', ';(', ';}', ':-D'])) // Expected Answer: 2
+// console.log(countSmileys([';]', ':[', ';*', ':$', ';-D'])) // Expected Answer: 1
+// console.log(countSmileys([':)',':(',':D',':O',':;'])) // Expected Answer: 2
+// console.log(countSmileys([':-)',';~D',':-D',':_D'])) // Expected Answer: 3
+// console.log(countSmileys([':---)','))',';~~D',';D'])) // Expected Answer: 1
+// console.log(countSmileys([';~)',':)',':-)',':--)'])) // Expected Answer: 3
+// console.log(countSmileys([':o)',':--D',';-~)'])) // Expected Answer: 0
+
+
+
